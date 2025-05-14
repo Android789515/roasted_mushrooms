4c4a50c38a868143a02565924f35405f34af01a4
Android789515 <derflug@mailfence.com>
2025-05-13 19:13:18 -0500

    feat: roasted mushroom items
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/main/java/android789515/roasted_mushrooms/RoastedMushrooms.java
+++ src/main/java/android789515/roasted_mushrooms/RoastedMushrooms.java
@@ -1,6 +1,14 @@
 package android789515.roasted_mushrooms;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +26,37 @@ public class RoastedMushrooms implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+      RegistryKey<Item> roastedBrownMushroom = RegistryKey.of(
+         RegistryKeys.ITEM,
+         Identifier.of(MOD_ID, "roasted_brown_mushroom")
+      );
+
+      final Item ROASTED_BROWN_MUSHROOM = new Item(new Item.Settings());
+      
+      Registry.register(
+            Registries.ITEM,
+            roastedBrownMushroom,
+            ROASTED_BROWN_MUSHROOM
+      );
+      
+      RegistryKey<Item> roastedRedMushroom = RegistryKey.of(
+         RegistryKeys.ITEM,
+         Identifier.of(MOD_ID, "roasted_red_mushroom")
+      );
+
+      final Item ROASTED_RED_MUSHROOM = new Item(new Item.Settings());
+
+      Registry.register(
+         Registries.ITEM,
+         roastedRedMushroom,
+         ROASTED_RED_MUSHROOM
+      );
+
+      ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK)
+         .register((itemGroup) -> {
+            itemGroup.add(ROASTED_BROWN_MUSHROOM);
+            itemGroup.add(ROASTED_RED_MUSHROOM);
+         });
 
 		LOGGER.info("Hello Fabric world!");
 	}
